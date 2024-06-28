a796dfdbde4f4d30bafe2f85c5e6c035ef1fcd8a
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-06-29 01:40:47 +0500

    chore: add more images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,51 +9,345 @@ import Image07 from "@/assets/images/image-07.jpg";
 import Image08 from "@/assets/images/image-08.jpg";
 import Image09 from "@/assets/images/image-09.jpg";
 import Image10 from "@/assets/images/image-10.jpg";
+import Image11 from "@/assets/images/image-11.jpg";
+import Image12 from "@/assets/images/image-12.jpg";
+import Image13 from "@/assets/images/image-13.png";
+import Image14 from "@/assets/images/image-14.jpg";
+import Image15 from "@/assets/images/image-15.jpg";
+import Image16 from "@/assets/images/image-16.jpg";
+import Image17 from "@/assets/images/image-17.jpg";
+import Image18 from "@/assets/images/image-18.jpg";
+import Image19 from "@/assets/images/image-19.jpg";
+import Image20 from "@/assets/images/image-20.jpg";
+import Image21 from "@/assets/images/image-21.jpg";
+import Image22 from "@/assets/images/image-22.jpg";
+import Image23 from "@/assets/images/image-23.jpg";
+import Image24 from "@/assets/images/image-24.jpg";
+import Image25 from "@/assets/images/image-25.png";
+import Image26 from "@/assets/images/image-26.png";
+import Image27 from "@/assets/images/image-27.jpg";
+import Image28 from "@/assets/images/image-28.jpg";
+import Image29 from "@/assets/images/image-29.jpg";
+import Image30 from "@/assets/images/image-30.jpg";
+import Image31 from "@/assets/images/image-31.jpg";
+import Image32 from "@/assets/images/image-32.jpg";
+import Image33 from "@/assets/images/image-33.jpg";
+import Image34 from "@/assets/images/image-34.jpg";
+import Image35 from "@/assets/images/image-35.jpg";
+import Image36 from "@/assets/images/image-36.png";
+import Image37 from "@/assets/images/image-37.png";
+import Image38 from "@/assets/images/image-38.jpg";
+import Image39 from "@/assets/images/image-39.png";
+import Image40 from "@/assets/images/image-40.png";
+import Image41 from "@/assets/images/image-41.jpg";
+
+const images = [
+  {
+    key: 1,
+    path: Image01,
+    alt: "Code 01",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 2,
+    path: Image02,
+    alt: "Code 02",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 3,
+    path: Image03,
+    alt: "Code 03",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 4,
+    path: Image04,
+    alt: "Code 04",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 5,
+    path: Image05,
+    alt: "Code 05",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 6,
+    path: Image06,
+    alt: "Code 06",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 7,
+    path: Image07,
+    alt: "Code 07",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 8,
+    path: Image08,
+    alt: "Code 08",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 9,
+    path: Image09,
+    alt: "Code 09",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 10,
+    path: Image10,
+    alt: "Code 10",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 11,
+    path: Image11,
+    alt: "Code 11",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 12,
+    path: Image12,
+    alt: "Code 12",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 13,
+    path: Image13,
+    alt: "Code 13",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 14,
+    path: Image14,
+    alt: "Code 14",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 15,
+    path: Image15,
+    alt: "Code 15",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 16,
+    path: Image16,
+    alt: "Code 16",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 17,
+    path: Image17,
+    alt: "Code 17",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 18,
+    path: Image18,
+    alt: "Code 18",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 19,
+    path: Image19,
+    alt: "Code 19",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 20,
+    path: Image20,
+    alt: "Code 20",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 21,
+    path: Image21,
+    alt: "Code 21",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 22,
+    path: Image22,
+    alt: "Code 22",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 23,
+    path: Image23,
+    alt: "Code 23",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 24,
+    path: Image24,
+    alt: "Code 24",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 25,
+    path: Image25,
+    alt: "Code 25",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 26,
+    path: Image26,
+    alt: "Code 26",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 27,
+    path: Image27,
+    alt: "Code 27",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 28,
+    path: Image28,
+    alt: "Code 28",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 29,
+    path: Image29,
+    alt: "Code 29",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 30,
+    path: Image30,
+    alt: "Code 30",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 31,
+    path: Image31,
+    alt: "Code 31",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 32,
+    path: Image32,
+    alt: "Code 32",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 33,
+    path: Image33,
+    alt: "Code 33",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 34,
+    path: Image34,
+    alt: "Code 34",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 35,
+    path: Image35,
+    alt: "Code 35",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 36,
+    path: Image36,
+    alt: "Code 36",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 37,
+    path: Image37,
+    alt: "Code 37",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 38,
+    path: Image38,
+    alt: "Code 38",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 39,
+    path: Image39,
+    alt: "Code 39",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 40,
+    path: Image40,
+    alt: "Code 40",
+    width: 1280,
+    height: 853,
+  },
+  {
+    key: 41,
+    path: Image41,
+    alt: "Code 41",
+    width: 1280,
+    height: 853,
+  },
+];
 
 export default function Home() {
   return (
     <section>
       <div className="container">
         <div className="grid sm:grid-cols-2 gap-x-4 gap-y-6 pt-8">
-          <article>
-            <Image src={Image01} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image02} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image03} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image04} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image05} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image06} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image07} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image08} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image09} alt="" width={1280} height={853} priority />
-          </article>
-
-          <article>
-            <Image src={Image10} alt="" width={1280} height={853} priority />
-          </article>
+          {images.map((image) => (
+            <article key={image.key}>
+              <Image
+                src={image.path}
+                alt={image.alt}
+                width={image.width}
+                height={image.height}
+                priority
+              />
+              <p>{image.alt}</p>
+            </article>
+          ))}
         </div>
       </div>
     </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
       center: true,
@@ -14,14 +10,9 @@ const config: Config = {
         "2xl": "1400px",
       },
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
